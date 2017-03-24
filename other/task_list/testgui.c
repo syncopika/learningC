d3bb4fb68d8629ec805145b1b080c15964c3367f
Nicholas H <syncopika@users.noreply.github.com>
2017-03-24 10:31:25 -0400

    Update testgui.c

--- other/task_list/testgui.c
+++ other/task_list/testgui.c
@@ -21,35 +21,6 @@ const char g_szClassName[] = "myWindowClass";
 static Heap *my_heap; /* this variable does not stay updated! */
 static HashMap *temp;
 
-/* this is a bit verbose for my purposes... 
-   but the important part is AllocConsole()
-
-#include <io.h>
-#include <fcntl.h>
-
-int APIENTRY WinMain(HINSTANCE hInstance,
-                     HINSTANCE hPrevInstance,
-                     LPSTR     lpCmdLine,
-                     int       nCmdShow)
-{
-    AllocConsole();
-
-    HANDLE handle_out = GetStdHandle(STD_OUTPUT_HANDLE);
-    int hCrt = _open_osfhandle((long) handle_out, _O_TEXT);
-    FILE* hf_out = _fdopen(hCrt, "w");
-    setvbuf(hf_out, NULL, _IONBF, 1);
-    *stdout = *hf_out;
-
-    HANDLE handle_in = GetStdHandle(STD_INPUT_HANDLE);
-    hCrt = _open_osfhandle((long) handle_in, _O_TEXT);
-    FILE* hf_in = _fdopen(hCrt, "r");
-    setvbuf(hf_in, NULL, _IONBF, 128);
-    *stdin = *hf_in;
-
-    // use the console just like a normal one - printf(), getchar(), ...
-}
-*/
-
 /* the window procedure */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
 	
